a2a76a5d90c803e0f54f874efb5bdc1abcb8634f
Adam Misik <adammisik@Adams-MacBook-Air.fritz.box>
2022-04-08 10:20:43 +0200

    Upload local dir

---
+++ src/process.py
@@ -0,0 +1,14 @@
+import hydra
+from omegaconf import DictConfig
+from hydra.utils import to_absolute_path as abspath
+
+@hydra.main(config_path="../config", config_name='main')
+def process_data(config: DictConfig):
+    """Function to process the data"""
+
+    raw_path = abspath(config.raw.path)
+    print(f"Process data using {raw_path}")
+    print(f"Columns used: {config.process.use_columns}")
+
+if __name__ == '__main__':
+    process_data()
